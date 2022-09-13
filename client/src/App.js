2fa4c5d68ed75c24dbe383d4921533aed9f13edb
Dmytro Kravtsov <dmytro.kravtsov@unicorn.com>
2022-09-13 11:50:35 +0200

    Initial setup commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const classroom = {
+  name: "Septima"
+};
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Classroom {classroom} </h1>
+    </div>
+  );
+}
+
+export default App;
